a04284b167924bff83be762a2c045fa9a9ea7758
root <root@31991.hostserv.eu>
2020-11-16 20:09:17 +0000

    "adminconsole"

---
+++ bergsolawi/wsgi.py
@@ -0,0 +1,15 @@
+"""
+WSGI config for bergsolawi project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bergsolawi.settings")
+
+application = get_wsgi_application()
+

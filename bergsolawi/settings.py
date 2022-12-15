5ecb166533ca605c26f858c42c902cbab19d86f8
Noetter <42866569+Noetter@users.noreply.github.com>
2022-12-15 13:26:19 +0100

    Update settings.py

--- bergsolawi/settings.py
+++ bergsolawi/settings.py
@@ -103,7 +103,7 @@ MIDDLEWARE = [
 EMAIL_HOST = os.environ.get('JUNTAGRICO_EMAIL_HOST')
 EMAIL_HOST_USER = os.environ.get('JUNTAGRICO_EMAIL_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('JUNTAGRICO_EMAIL_PASSWORD')
-EMAIL_PORT = int(os.environ.get('JUNTAGRICO_EMAIL_PORT', '25' ))
+EMAIL_PORT = int(os.environ.get('JUNTAGRICO_EMAIL_PORT', '993' ))
 EMAIL_USE_TLS = os.environ.get('JUNTAGRICO_EMAIL_TLS', 'False')=='True'
 EMAIL_USE_SSL = os.environ.get('JUNTAGRICO_EMAIL_SSL', 'False')=='True'
 

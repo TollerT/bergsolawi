55ab685306dacdfdf9b448504bb91e042c1daed5
Theo von Arx <varxth@ethz.ch>
2024-02-16 14:53:53 +0100

    Format file

--- bergsolawi/settings.py
+++ bergsolawi/settings.py
@@ -10,68 +10,79 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('JUNTAGRICO_SECRET_KEY')
+SECRET_KEY = os.environ.get("JUNTAGRICO_SECRET_KEY")
 
-DEBUG = os.environ.get("JUNTAGRICO_DEBUG", 'False')=='True'
+DEBUG = os.environ.get("JUNTAGRICO_DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = ['bergsolawi-surselva.ch', 'localhost',]
+ALLOWED_HOSTS = [
+    "bergsolawi-surselva.ch",
+    "localhost",
+]
 
 
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.sites',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'django.contrib.admin',
-    'juntagrico',
-    'impersonate',
-    'crispy_forms',
-    'bergsolawi',
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.sites",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "django.contrib.admin",
+    "juntagrico",
+    "impersonate",
+    "crispy_forms",
+    "bergsolawi",
 ]
 
-ROOT_URLCONF = 'bergsolawi.urls'
+ROOT_URLCONF = "bergsolawi.urls"
 
 DATABASES = {
-    'default': {
-        'ENGINE': os.environ.get('JUNTAGRICO_DATABASE_ENGINE','django.db.backends.sqlite3'), 
-        'NAME': os.environ.get('JUNTAGRICO_DATABASE_NAME','bergsolawi.db'), 
-        'USER': os.environ.get('JUNTAGRICO_DATABASE_USER'), #''junatagrico', # The following settings are not used with sqlite3:
-        'PASSWORD': os.environ.get('JUNTAGRICO_DATABASE_PASSWORD'), #''junatagrico',
-        'HOST': os.environ.get('JUNTAGRICO_DATABASE_HOST'), #'localhost', # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': os.environ.get('JUNTAGRICO_DATABASE_PORT', False), #''', # Set to empty string for default.
+    "default": {
+        "ENGINE": os.environ.get(
+            "JUNTAGRICO_DATABASE_ENGINE", "django.db.backends.sqlite3"
+        ),
+        "NAME": os.environ.get("JUNTAGRICO_DATABASE_NAME", "bergsolawi.db"),
+        "USER": os.environ.get(
+            "JUNTAGRICO_DATABASE_USER"
+        ),  #''junatagrico', # The following settings are not used with sqlite3:
+        "PASSWORD": os.environ.get("JUNTAGRICO_DATABASE_PASSWORD"),  #''junatagrico',
+        "HOST": os.environ.get(
+            "JUNTAGRICO_DATABASE_HOST"
+        ),  #'localhost', # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        "PORT": os.environ.get(
+            "JUNTAGRICO_DATABASE_PORT", False
+        ),  #''', # Set to empty string for default.
     }
 }
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'OPTIONS': {
-            'context_processors': [
-                'django.contrib.auth.context_processors.auth',
-                'django.template.context_processors.debug',
-                'django.template.context_processors.i18n',
-                'django.template.context_processors.media',
-                'django.template.context_processors.static',
-                'django.template.context_processors.tz',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "OPTIONS": {
+            "context_processors": [
+                "django.contrib.auth.context_processors.auth",
+                "django.template.context_processors.debug",
+                "django.template.context_processors.i18n",
+                "django.template.context_processors.media",
+                "django.template.context_processors.static",
+                "django.template.context_processors.tz",
+                "django.contrib.messages.context_processors.messages",
             ],
-            'loaders': [
-                'django.template.loaders.filesystem.Loader',
-                'django.template.loaders.app_directories.Loader'
+            "loaders": [
+                "django.template.loaders.filesystem.Loader",
+                "django.template.loaders.app_directories.Loader",
             ],
-            'debug' : True
+            "debug": True,
         },
     },
 ]
 
-WSGI_APPLICATION = 'bergsolawi.wsgi.application'
+WSGI_APPLICATION = "bergsolawi.wsgi.application"
 
 
-LANGUAGE_CODE = 'de'
+LANGUAGE_CODE = "de"
 
 SITE_ID = 1
 
@@ -83,52 +94,54 @@ USE_I18N = True
 # calendars according to the current locale.
 USE_L10N = True
 
-DATE_INPUT_FORMATS =['%d.%m.%Y',]
+DATE_INPUT_FORMATS = [
+    "%d.%m.%Y",
+]
 
 AUTHENTICATION_BACKENDS = (
-    'juntagrico.util.auth.AuthenticateWithEmail',
-    'django.contrib.auth.backends.ModelBackend'
+    "juntagrico.util.auth.AuthenticateWithEmail",
+    "django.contrib.auth.backends.ModelBackend",
 )
 
 
 MIDDLEWARE = [
-    'django.middleware.common.CommonMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'impersonate.middleware.ImpersonateMiddleware'
+    "django.middleware.common.CommonMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "impersonate.middleware.ImpersonateMiddleware",
 ]
 
-EMAIL_HOST = os.environ.get('JUNTAGRICO_EMAIL_HOST')
-EMAIL_HOST_USER = os.environ.get('JUNTAGRICO_EMAIL_USER')
-EMAIL_HOST_PASSWORD = os.environ.get('JUNTAGRICO_EMAIL_PASSWORD')
-EMAIL_PORT = int(os.environ.get('JUNTAGRICO_EMAIL_PORT', '25' ))
-EMAIL_USE_TLS = os.environ.get('JUNTAGRICO_EMAIL_TLS', 'False')=='True'
-EMAIL_USE_SSL = os.environ.get('JUNTAGRICO_EMAIL_SSL', 'False')=='True'
+EMAIL_HOST = os.environ.get("JUNTAGRICO_EMAIL_HOST")
+EMAIL_HOST_USER = os.environ.get("JUNTAGRICO_EMAIL_USER")
+EMAIL_HOST_PASSWORD = os.environ.get("JUNTAGRICO_EMAIL_PASSWORD")
+EMAIL_PORT = int(os.environ.get("JUNTAGRICO_EMAIL_PORT", "25"))
+EMAIL_USE_TLS = os.environ.get("JUNTAGRICO_EMAIL_TLS", "False") == "True"
+EMAIL_USE_SSL = os.environ.get("JUNTAGRICO_EMAIL_SSL", "False") == "True"
 
-SESSION_SERIALIZER = 'django.contrib.sessions.serializers.PickleSerializer'
+SESSION_SERIALIZER = "django.contrib.sessions.serializers.PickleSerializer"
 
 WHITELIST_EMAILS = []
+
 
 def whitelist_email_from_env(var_env_name):
     email = os.environ.get(var_env_name)
     if email:
-        WHITELIST_EMAILS.append(email.replace('@gmail.com', '(\+\S+)?@gmail.com'))
+        WHITELIST_EMAILS.append(email.replace("@gmail.com", "(\+\S+)?@gmail.com"))
 
 
 if DEBUG is True:
     for key in os.environ.keys():
         if key.startswith("JUNTAGRICO_EMAIL_WHITELISTED"):
             whitelist_email_from_env(key)
-            
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_URL = "/static/"
 
 IMPERSONATE = {
-    'REDIRECT_URL': '/my/profile',
+    "REDIRECT_URL": "/my/profile",
 }
 
 LOGIN_REDIRECT_URL = "/my/home"
@@ -136,37 +149,40 @@ LOGIN_REDIRECT_URL = "/my/home"
 """
     File & Storage Settings
 """
-ADMIN_MEDIA_PREFIX = STATIC_URL + 'admin/'
+ADMIN_MEDIA_PREFIX = STATIC_URL + "admin/"
 
-MEDIA_ROOT = 'media'
+MEDIA_ROOT = "media"
 
 """
      Crispy Settings
 """
-CRISPY_TEMPLATE_PACK = 'bootstrap4'
+CRISPY_TEMPLATE_PACK = "bootstrap4"
 
 """
      juntagrico Settings
 """
 BUSINESS_YEAR_CANCELATION_MONTH = 2
 MEMBERSHIP_END_MONTH = 5
-BUSINESS_YEAR_START = {"day":1, "month":7}
+BUSINESS_YEAR_START = {"day": 1, "month": 7}
 
 
 ORGANISATION_NAME = "Bergsolawi"
 ORGANISATION_LONG_NAME = "Bergsolawi"
-ORGANISATION_ADDRESS = {"name":"Bergsolawi Surselva", 
-            "street" : "Letzigraben",
-            "number" : "39",
-            "zip" : "8003",
-            "city" : "Zürich",
-            "extra" : ""}
+ORGANISATION_ADDRESS = {
+    "name": "Bergsolawi Surselva",
+    "street": "Letzigraben",
+    "number": "39",
+    "zip": "8003",
+    "city": "Zürich",
+    "extra": "",
+}
 ORGANISATION_BANK_CONNECTION = {
-            "PC" : "123",
-            "IBAN" : "CH33 0839 0037 0378 1000 3",
-            "BIC" : "123",
-            "NAME" : "Alternative Bank Schweiz",
-            "ESR" : ""}
+    "PC": "123",
+    "IBAN": "CH33 0839 0037 0378 1000 3",
+    "BIC": "123",
+    "NAME": "Alternative Bank Schweiz",
+    "ESR": "",
+}
 SHARE_PRICE = "1111"
 
 INFO_EMAIL = "info@bergsolawi.ch"

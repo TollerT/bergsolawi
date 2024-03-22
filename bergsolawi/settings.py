e306656da02cd0a1eed86fef9518526986a2b9b0
reginaflury <159420012+reginaflury@users.noreply.github.com>
2024-03-22 19:13:02 +0100

    Update settings.py

--- bergsolawi/settings.py
+++ bergsolawi/settings.py
@@ -94,7 +94,7 @@ WSGI_APPLICATION = "bergsolawi.wsgi.application"
 
 LANGUAGE_CODE = "de"
 
-SITE_ID = 5
+SITE_ID = 6
 
 # If you set this to False, Django will make some optimizations so as not
 # to load the internationalization machinery.
